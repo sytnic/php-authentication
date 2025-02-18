be6ca6b8cbe6e96d856f14ab86d77138b93c1e70
sytnic <sytnic@list.ru>
2025-02-18 14:57:48 +0300

    15.1 Sql dump

---
+++ database/sql_dump.sql
@@ -0,0 +1,27 @@
+-- Adminer 4.8.1 MySQL 5.7.39 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+CREATE DATABASE `globe_bank` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `globe_bank`;
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hashed_password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `admins` (`id`, `first_name`, `last_name`, `username`, `email`, `hashed_password`) VALUES
+(1,	'Kevin',	'Skoglund',	'kskoglund',	'someone@nowhere.com',	'$2y$10$QQV5GWNxCrqI5ep6lFFcyeYv./AmkTslWXx70Y9gEKi.V.ObejmaK');
+
+-- 2025-02-18 11:53:58
